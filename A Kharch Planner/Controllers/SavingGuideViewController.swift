0e007cb077014bf0a0d547c1c0351c94aa656c0e
dk-talks <dksbansur8502@gmail.com>
2023-04-29 17:28:23 +0530

    Final commit

---
+++ A Kharch Planner/Controllers/SavingGuideViewController.swift
@@ -0,0 +1,64 @@
+//
+//  SavingGuideViewController.swift
+//  A Kharch Planner
+//
+//  Created by Dinesh Sharma on 28/04/23.
+//
+
+import UIKit
+
+class SavingGuideViewController: UIViewController {
+
+    @IBOutlet weak var txtView: UITextView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+            
+        txtView.text = """
+        
+        A Comprehensive Guide to Saving Money and Achieving Financial Stability
+
+        Introduction:
+
+        Saving money is an essential skill that everyone should learn. Regardless of how much you earn, managing your finances effectively can help you achieve your financial goals and improve your quality of life. In this guide, we will discuss some practical tips and strategies that can help you save money and achieve financial stability.
+
+        Set Realistic Goals
+
+        Before you start saving money, it's essential to set realistic goals. Think about what you want to achieve and how much money you need to save to reach your goals. Whether it's buying a new car, going on vacation, or paying off debt, having a specific goal in mind can help you stay motivated and focused.
+
+        Create a Budget
+
+        Creating a budget is an effective way to manage your finances and save money. Start by tracking your income and expenses and identifying areas where you can cut back on unnecessary spending. Make sure to allocate funds for essential expenses such as rent, utilities, and groceries, and set aside some money for savings.
+
+        Reduce Your Debt
+
+        Debt can be a significant obstacle to achieving financial stability. If you have debt, focus on paying it off as quickly as possible. Start by prioritizing high-interest debt such as credit cards and personal loans. Consider consolidating your debt or negotiating with creditors to reduce your interest rates.
+
+        Cut Back on Unnecessary Expenses
+
+        Reducing your expenses is an effective way to save money. Start by reviewing your monthly bills and subscriptions and canceling any services that you don't use or need. Consider switching to a cheaper cell phone plan or reducing your cable TV package. You can also save money by cutting back on dining out and entertainment expenses.
+
+        Find Ways to Increase Your Income
+
+        Increasing your income is another effective way to save money. Consider taking on a part-time job or freelance work to supplement your income. You can also sell unwanted items online or rent out a spare room on Airbnb. Look for opportunities to turn your hobbies or skills into a side business.
+
+        Invest in Your Future
+
+        Investing in your future is essential for achieving long-term financial stability. Consider opening a retirement account or investing in stocks, bonds, or mutual funds. Make sure to do your research and consult with a financial advisor before making any investment decisions.
+
+        Conclusion:
+
+        Saving money is a continuous process that requires discipline, patience, and commitment. By setting realistic goals, creating a budget, reducing your debt, cutting back on unnecessary expenses, finding ways to increase your income, and investing in your future, you can achieve financial stability and enjoy a better quality of life. Start implementing these tips today, and watch your savings grow over time.
+        
+        """
+        
+        
+    }
+    
+    
+    
+
+    
+
+}
--- A Kharch Planner/Controllers/ViewController.swift
+++ A Kharch Planner/Controllers/ViewController.swift
@@ -20,13 +20,13 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         
         txtView.text = """
-        Hey There!
-        How Are You Doing Today?
-        We have brought you an amazing application to track your expenses.
-        Using this app will enable you to limit your budget as you want and as the days pass you can enter your expenses in the app.
-        This will help you remind of over-spending and at the same time you can analyze your budget & expenses with the help of charts.
-        Give It a try!
-        We Hope you Enjoy It!
+        Introducing our new expense management app, designed to help you take control of your finances and manage your budget with ease. Our app is perfect for anyone who wants to keep track of their expenses, set a budget, and make informed financial decisions.
+
+        With our app, you can easily input all of your income and expenses, and categorize them as necessary. Our simple and intuitive interface makes it easy to add new transactions and keep track of your spending in real-time. You can also set a budget for each category and receive notifications when you are getting close to your limit.
+
+        Our app provides detailed reports and visual representations of your spending habits. You can see how much money you spend in each category, how much you have left in your budget, and how your spending habits have changed over time. With this information, you can make informed decisions about how to adjust your spending and manage your finances more effectively.
+
+        Download our app today and take the first step towards a more organized and financially stable future.
         """
         
         imgLogo.layer.cornerRadius = imgLogo.frame.size.width/2
@@ -42,49 +42,40 @@ class ViewController: UIViewController {
         }
         
     }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        self.navigationController?.isNavigationBarHidden = true
+    }
+    override func viewWillDisappear(_ animated: Bool) {
+        self.navigationController?.isNavigationBarHidden = false
+    }
 
     @IBAction func btnAgreeTapped(_ sender: Any) {
         defaults.set(true, forKey: "isProceed")
         
         let budget1 = Budget(context: Global.context)
-        budget1.name = "Food"
-        budget1.amount = 350
+        budget1.name = "Other"
+        budget1.amount = 0
         
         let budget2 = Budget(context: Global.context)
-        budget2.name = "Family"
-        budget2.amount = 500
-        
-        let budget3 = Budget(context: Global.context)
-        budget3.name = "Partying"
-        budget3.amount = 350
-        
-        let budget4 = Budget(context: Global.context)
-        budget4.name = "Gadgets"
-        budget4.amount = 100
+        budget2.name = "Stay"
+        budget2.amount = 0
         
         let budget5 = Budget(context: Global.context)
-        budget5.name = "Other"
-        budget5.amount = 350
+        budget5.name = "Food"
+        budget5.amount = 0
         
         let expense1 = Expense(context: Global.context)
-        expense1.name = "Food"
+        expense1.name = "Other"
         expense1.amount = 0
         
         let expense2 = Expense(context: Global.context)
-        expense2.name = "Family"
+        expense2.name = "Stay"
         expense2.amount = 0
         
         let expense3 = Expense(context: Global.context)
-        expense3.name = "Partying"
+        expense3.name = "Food"
         expense3.amount = 0
-        
-        let expense4 = Expense(context: Global.context)
-        expense4.name = "Gadgets"
-        expense4.amount = 0
-        
-        let expense5 = Expense(context: Global.context)
-        expense5.name = "Other"
-        expense5.amount = 0
         
         do {
             try Global.context.save()
